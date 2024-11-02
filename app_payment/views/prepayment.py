70f269df1a0625b64c445aaf085cfaf4ae8ae8e2
maloy-baroi <maloy.virat@gmail.com>
2024-11-03 00:46:43 +0600

    bug: removed all authentication mandatory

--- app_payment/views/prepayment.py
+++ app_payment/views/prepayment.py
@@ -21,29 +21,34 @@ class PrePaymentCreateListAPIView(APIView):
             return Response({"message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
     def post(self, request, *args, **kwargs):
-        data = request.data.copy()  # Copy the request data to make it mutable
+        try:
+            data = request.data.copy()  # Copy the request data to make it mutable
 
-        # Handle base64 encoded image if present
-        payment_screenshot_data = data.get('payment_screenshot')
-        if payment_screenshot_data:
-            format, imgstr = payment_screenshot_data.split(';base64,')  # Extract format and pure base64 data
-            ext = format.split('/')[-1]  # Extract the extension (png, jpeg, etc.)
+            # Handle base64 encoded image if present
+            payment_screenshot_data = data.get('payment_screenshot')
+            if payment_screenshot_data:
+                format, imgstr = payment_screenshot_data.split(';base64,')  # Extract format and pure base64 data
+                ext = format.split('/')[-1]  # Extract the extension (png, jpeg, etc.)
 
-            # Convert base64 encoded data into an image file
-            data['payment_screenshot'] = ContentFile(base64.b64decode(imgstr), name=f"temp.{ext}")
+                # Convert base64 encoded data into an image file
+                data['payment_screenshot'] = ContentFile(base64.b64decode(imgstr), name=f"temp.{ext}")
 
-        serializer = PrePaymentSerializer(data=data)
+            serializer = PrePaymentSerializer(data=data)
 
-        if serializer.is_valid():
-            currency_rate = CurrencyExchangeRate.objects.get(source_from=data['currency'], converted_to='BDT')
-            in_bdt = currency_rate.amount_per_unit * float(data['amount'])
-            serializer.save(
-                commission=0,
-                after_commission=data['amount'],
-                balance=data['amount'],
-                in_bdt=in_bdt
-            )  # Save the new Payment instance
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            if serializer.is_valid():
+                # currency_rate = CurrencyExchangeRate.objects.get(source_from=data['currency'], converted_to='BDT')
+                # in_bdt = currency_rate.amount_per_unit * float(data['amount'])
+                serializer.save(
+                    commission=0,
+                    after_commission=data['amount'],
+                    balance=data['amount']
+                )  # Save the new Payment instance
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                print(serializer.errors)
+                return Response({"message": str(serializer.errors)}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            print(str(e))
+            return Response({"message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 
